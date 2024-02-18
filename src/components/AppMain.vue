d55f61a2d4a749977be09888f7235d503cbde0db
cristinagiacobbe <crigiak@gmail.com>
2024-02-18 11:08:03 +0100

    Initial set up

---
+++ src/components/AppMain.vue
@@ -0,0 +1,14 @@
+<script>
+import { state } from '../state.js'
+
+export default {
+    name: 'AppMain',
+    data() {
+        return {
+            state,
+        }
+    }
+}
+</script>
+<template></template>
+<style></style>
